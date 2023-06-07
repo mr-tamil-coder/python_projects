48bf43443ec60bf3b20875c14799f0ed9b9c130a
Vinoth <131464734+mr-tamil-coder@users.noreply.github.com>
2023-06-07 19:30:28 +0530

    Add files via upload

---
+++ 01 gcd.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter first number"))
+num2=int(input("Enter Second number"))
+if num1<num2:
+  smaller=num1
+else:
+  smaller=num2
+for i in range(1,smaller+1):
+  if(num1%i==0)and(num2%i==0):
+    gcd=i
+print("The G.C.D of ",num1,"and",num2,"is =",gcd)
